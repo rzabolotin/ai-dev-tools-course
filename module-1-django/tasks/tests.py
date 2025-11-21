3a5facbec548fd56bb2349f04fb4835860b0d016
Roman Zabolotin <rzabolotin@gmail.com>
2025-11-22 12:33:20 +1300

    add tests

--- module-1-django/tasks/tests.py
+++ module-1-django/tasks/tests.py
@@ -37,3 +37,102 @@ class TaskViewTest(TestCase):
         })
         self.assertEqual(response.status_code, 200) # Renders form again
         self.assertFalse(Task.objects.filter(description='New Description').exists())
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.task = Task.objects.create(
+            user=self.user,
+            title='Test Task',
+            description='Test Description'
+        )
+
+    def test_task_str(self):
+        self.assertEqual(str(self.task), 'Test Task')
+
+    def test_task_defaults(self):
+        self.assertFalse(self.task.is_done)
+        self.assertIsNotNone(self.task.created_at)
+
+class TaskListViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.task = Task.objects.create(user=self.user, title='Test Task')
+
+    def test_task_list_view(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Task')
+        self.assertTemplateUsed(response, 'tasks/task_list.html')
+
+class TaskEditViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.task = Task.objects.create(user=self.user, title='Test Task')
+
+    def test_task_edit_view(self):
+        response = self.client.post(reverse('task_edit', args=[self.task.pk]), {
+            'title': 'Updated Task',
+            'description': 'Updated Description',
+            'due_date': '2023-12-31'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.title, 'Updated Task')
+
+class TaskDeleteViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.task = Task.objects.create(user=self.user, title='Test Task')
+
+    def test_task_delete_view(self):
+        response = self.client.post(reverse('task_delete', args=[self.task.pk]))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Task.objects.filter(pk=self.task.pk).exists())
+
+class TaskToggleViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.task = Task.objects.create(user=self.user, title='Test Task', is_done=False)
+
+    def test_task_toggle_view(self):
+        response = self.client.post(reverse('task_toggle', args=[self.task.pk]))
+        self.assertEqual(response.status_code, 302)
+        self.task.refresh_from_db()
+        self.assertTrue(self.task.is_done)
+
+class AuthViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_register_view(self):
+        response = self.client.post(reverse('register'), {
+            'username': 'newuser',
+            'password1': 'ComplexPassword123!',
+            'password2': 'ComplexPassword123!'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    def test_login_view(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': 'testpassword'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_logout_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
