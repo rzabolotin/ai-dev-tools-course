1574075d1bce630e4df544c2be3cd640c9bc0b7c
Roman Zabolotin <rzabolotin@gmail.com>
2025-11-22 13:52:41 +1300

    add task to admin

--- module-1-django/tasks/admin.py
+++ module-1-django/tasks/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'due_date', 'is_done', 'created_at')
+    list_filter = ('is_done', 'user', 'due_date')
+    search_fields = ('title', 'description')
+    list_editable = ('is_done',)
+    readonly_fields = ('created_at',)
+    
+    fieldsets = (
+        ('Task Information', {
+            'fields': ('title', 'description', 'user')
+        }),
+        ('Status & Dates', {
+            'fields': ('is_done', 'due_date', 'created_at')
+        }),
+    )
